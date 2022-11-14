d11d86c817c3ee3cf6ff6bcde03c56a9c33edaa0
Vlad Vynnyk <vladvynnyk1@gmail.com>
2022-11-14 11:33:50 +0200

    Added buttons for sorting array and created array for tasks. Also updated functions for adding elements.

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+let tasks = [{"task":"Dishes", "isComplete":false, "dueDate":"2021-01-01T15:45:00.000Z"},{"task":"feed baby", "isComplete":true, "dueDate":"2021-01-02T15:45:00.000Z"},{"task":"Laundry", "isComplete":true, "dueDate":"2021-01-04T15:45:00.000Z"},{"task":"Homework", "isComplete":false, "dueDate":"2021-01-03T15:45:00.000Z"}]
+
+
+tasks.forEach((el)=>{
+  var li = document.createElement("li");
+  var inputValue = el.task;
+  let date = el.dueDate.toString()
+  date.slice(0, 4)
+  var t = document.createTextNode(inputValue);
+  var d = document.createTextNode(date)
+  li.appendChild(t);
+  li.appendChild(d);
+    document.getElementById("container").appendChild(li);
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+})
+
 // Create a "close" button and append it to each list item
 var myNodelist = document.getElementsByTagName("LI");
 var i;
@@ -31,12 +53,18 @@ list.addEventListener('click', function(ev) {
 function newElement() {
   var li = document.createElement("li");
   var inputValue = document.getElementById("myInput").value;
+  let today = new Date()
+  let elem = {'task': inputValue, "isComplete":false, "dueDate":today}
+  tasks.push(elem)
   var t = document.createTextNode(inputValue);
+  var d = document.createTextNode(today.toLocaleDateString("en-US"))
   li.appendChild(t);
+  li.appendChild(d);
+  console.log(tasks)
   if (inputValue === '') {
     alert("You must write something!");
   } else {
-    document.getElementById("myUL").appendChild(li);
+    document.getElementById("container").appendChild(li);
   }
   document.getElementById("myInput").value = "";
 
@@ -52,4 +80,17 @@ function newElement() {
       div.style.display = "none";
     }
   }
+}
+
+function sortElems(){
+  let els = document.getElementsByClassName("elem")
+  Array.from(els).forEach((el) => {
+
+    if(el.classList.contains("checked")){
+      let clonedElem=el.cloneNode(true)
+      document.querySelector("container").appendChild(clonedElem)
+
+    }
+  
+  })
 }
